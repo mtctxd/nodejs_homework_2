eba451bceaa169dd36bd66ca6296b30836983997
Dmytro Osypiuk <Dmytro_Osypiuk@epam.com>
2022-07-14 09:17:22 +0200

    project restart

--- src/v1/middlewares/validator/schemes/index.ts
+++
@@ -1,9 +0,0 @@
-import { groupValidationsSchema } from "./groupSchema";
-import { userValidationsSchema } from "./userSchema";
-
-export type ValidatorSchema = typeof validationShcemes.user | typeof validationShcemes.group;
-
-export const validationShcemes = {
-  user: userValidationsSchema,
-  group: groupValidationsSchema,
-};
